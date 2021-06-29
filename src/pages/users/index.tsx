2addbccb03f6a5a004d3f6ba67ace886bd1006ba
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2021-06-28 22:00:12 -0300

    Configurando Axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,14 +3,15 @@ import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody
 import { RiAddLine, RiPencilLine, RiRefreshLine } from 'react-icons/ri';
 import { useQuery } from 'react-query';
 
+import { api } from '../../services/api';
+
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://192.168.0.106:3000/api/users')
-    const data = await response.json();
+    const { data } = await api.get('/users');
 
     const users = data.users.map(user => {
       return {
