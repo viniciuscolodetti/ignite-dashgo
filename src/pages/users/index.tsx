3a5afa1a3acca772184edc6c946c333804d3eba9
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2021-06-28 21:54:43 -0300

    Sinalizando refetch dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from '@chakra-ui/react';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { RiAddLine, RiPencilLine, RiRefreshLine } from 'react-icons/ri';
 import { useQuery } from 'react-query';
 
 import { Header } from '../../components/Header';
@@ -8,8 +8,8 @@ import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const response = await fetch('http://192.168.0.106:3000/api/users')
     const data = await response.json();
 
     const users = data.users.map(user => {
@@ -43,9 +43,12 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
+            </Heading>
 
-            <Link href="/users/create" passHref>
+            <Link href="/users/create" passHref>              
               <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
                 Criar novo
               </Button>
